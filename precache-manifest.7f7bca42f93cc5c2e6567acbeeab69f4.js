782adb41cc4902bee0fa58a73344ce9497a0a546
Faraz Saudagar <shadowrider923@gmail.com>
2022-01-12 10:10:41 -0500

    Updates

---
+++ precache-manifest.7f7bca42f93cc5c2e6567acbeeab69f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d452ebed72040ba828afd98ffc2edff",
+    "url": "/book-list/index.html"
+  },
+  {
+    "revision": "05fc00fa5b3cb2cdadd9",
+    "url": "/book-list/static/css/main.4d349b50.chunk.css"
+  },
+  {
+    "revision": "dca5cf80b922063a8eb5",
+    "url": "/book-list/static/js/2.caa8e877.chunk.js"
+  },
+  {
+    "revision": "05fc00fa5b3cb2cdadd9",
+    "url": "/book-list/static/js/main.f13d11fc.chunk.js"
+  },
+  {
+    "revision": "370557dc34b895d25c41",
+    "url": "/book-list/static/js/runtime~main.88ddd283.js"
+  }
+]);
